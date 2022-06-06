2dc40c2a738d67c6663cf81c533b66aa7e1200ee
stingraypcarter <stingraypcarter@gmail.com>
2022-06-05 22:57:28 -0400

    starting orgenv main

---
+++ orgenv.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Datiat
+{
+
+};
+
+class Avertiat
+{
+
+
+};
+
+class Fabriat
+{
+
+};
+
+
+class Morb
+{
+    public:
+    string name;
+    int id;
+
+    int cordigery;
+    int artanity;
+    int fluxibity;
+    int gorobancy;
+
+    // Morb *bonded;
+};
+
+
+
+int main() {
+
+    Morb tolarm;
+    tolarm.name = "tolarm";
+    tolarm.id = 1;
+
+    Morb palarm;
+    palarm.name = "palarm";
+    palarm.id = 2;
+
+    Morb filarm;
+    filarm.name = "filarm";
+    filarm.id = 3;
+
+    return 0;
+}
+
+
+
